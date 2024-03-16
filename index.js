8afcbff136f0ffc279470f4b6aef383d6561ff19
R3dH4wk44 <albert.programar@gmail.com>
2024-03-16 10:47:37 +0100

    Funciona el fetch de datos?

--- index.js
+++ index.js
@@ -23,14 +23,39 @@ window.onload = () => {
                 return res.json();
             })
             .then((data) => {
-                var productosJSON = JSON.parse(data);
+                pintarProductos(data);
                 console.log(data);
              })
-             .catch((error) => 
-                       console.error("Unable to fetch data:", error));
+             .catch((error) => {
+                       console.error("Unable to fetch data:", error)});
         }
 
+
         fetchJSON();
+        function pintarProductos(data){
+            let padre = document.getElementById('gridProductos');
+
+            let cadena = "";
+
+            data.forEach(producto => {
+                cadena += `<div class=" productGrid col bg-slate-100 mx-10 md:mx-0 shadow-md rounded-lg flex flex-col overflow-hidden" id="gridProductos">
+                <div class="productImg rounded-full"><img src="${producto.imagen}" class="w-full h-48 object-cover" alt=""></div>
+                <div class="titile py-5 px-2">
+                    <span class="text-xl font-bold">${producto.titulo}</span>
+                </div>
+                <div class="review flex px-2 py-0 align-middle items-center">
+                    <i class="fa-regular fa-star"></i>
+                    <span class="font-medium">&nbsp; ${producto.estrellas}</span><span class="font-light">${producto.valoraciones}</span>
+                </div>
+                <div class="price flex py-5 px-2 items-center justify-between">
+                    <span class="font-bold">${producto.precio} <b class="font-extrabold">€</b></span>
+                    <button class="rounded-full bg-green-500 buttonChart px-4 py-2 mx-10"><i class="fa-solid fa-cart-shopping"></i></button>
+                </div>
+            </div>`
+            });
+
+            padre.innerHTML += cadena;
+        }
      }
      
 
